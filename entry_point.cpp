57749b5bd98a8feb73cd2741bfeb6a2d31e0322a
Thomas Russel Carrel <annon>
2016-04-25 10:32:27 -0700

    Made it so that stdout is used instead when the same file is specified for input and output.

--- entry_point.cpp
+++ entry_point.cpp
@@ -23,25 +23,32 @@ int main(int argc, char* argv[])
 
     if( argc == 3 )
     {
-        if( !strcmp( argv[1], argv[2] ) )
+        if( strcmp( argv[1], argv[2] ) )
         {
-            std::cerr
-                << "\nInput and output files cannot be the same.\n" << std::endl;
-            return 1;
+
+            std::ofstream out;
+
+            out.open(argv[2]);
+
+            if( !out.good() )
+                return 1;
+
+
+            file.output(out);
+
+            out.close();
+
+            return 0;
         }
-        std::ofstream out;
-
-        out.open(argv[2]);
-
-        if( !out.good() )
-            return 1;
-
-
-        file.output(out);
-
-        out.close();
-
-        return 0;
+        else
+            std::cerr
+                << "-------------------------------------------"
+                << "---------------------------------------"
+                << "\nInput and output files cannot be the same.  "
+                << "Data will be output to stdout instead.\n"
+                << "-------------------------------------------"
+                << "---------------------------------------\n"
+                << std::endl;
     }
 
     file.output(std::cout);
