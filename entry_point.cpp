42b2741442dbd8ee7451c7d5e4da0df259df73ea
Thomas Russel Carrel <annon>
2016-04-25 09:45:38 -0700

    Added optional output to stdout.

--- entry_point.cpp
+++ entry_point.cpp
@@ -3,25 +3,34 @@
 
 #include"file_reader.h"
 
-using namespace std;
 
 int main(int argc, char* argv[])
 {
-    if( argc != 3 )
+    if( argc != 3 && argc != 2 )
     {
         return 1;
     }
 
     File file(argv[1]);
 
-    ofstream out;
+    if( argc == 3 )
+    {
+    std::ofstream out;
+
     out.open(argv[2]);
+
     if( !out.good() )
         return 1;
 
     file.output(out);
 
     out.close();
+
+    return 0;
+    }
+
+    file.output(std::cout);
+    std::cout << "\n" << std::endl;
 
     return 0;
 }
